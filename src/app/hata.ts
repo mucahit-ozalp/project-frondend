d17e6b5a951d81b4c2303e841f6a746ce738ffa1
Aybüke Üstün <aybuke0ustun@gmail.com>
2019-09-06 09:23:21 +0300

    düzeltme

--- src/app/hata.ts
+++ src/app/hata.ts
@@ -2,10 +2,11 @@ export class Hata {
     hatali_giris: string;
     kullanici_adi: string;
 
-    constructor(hatali_giris,kullanici_adi){
+    constructor(hatali_giris, kullanici_adi){
         
         this.hatali_giris=hatali_giris;
         this.kullanici_adi=kullanici_adi;
+        //deneme 123
         
     }
 }
